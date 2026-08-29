3dcbca310f489304cbac6955508c64540ca59186
Prince20052004 <princek1081@gmail.com>
2026-08-29 21:26:12 +0530

    Time: 0 ms (100%), Space: 9.1 MB (31.02%) - LeetHub

---
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int numDecodings(string s) {
+        int n=s.length();
+        if(n==0){
+            return 0;
+        }
+        vector<int> dp(n+1);
+        dp[0]=1;
+        dp[1]=s[0]=='0'? 0:1;
+        for(int i=2; i<=n; i++){
+            int onedigit=s[i-1]-'0';
+            int twodigit=stoi(s.substr(i-2, 2));
+            if(onedigit>=1){
+                dp[i]+=dp[i-1];
+            }
+            if(twodigit>=10 && twodigit<=26){
+                dp[i]+=dp[i-2];
+            }
+        }
+        return dp[n];
+    }
+};
