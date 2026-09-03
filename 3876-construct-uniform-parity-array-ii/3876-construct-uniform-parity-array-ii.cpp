7f4df967b3f390f7b6a69f0d1552097d2dbb58af
Prince20052004 <princek1081@gmail.com>
2026-09-03 18:31:06 +0530

    Time: 0 ms (100%), Space: 165.8 MB (62.83%) - LeetHub

---
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums1) {
+        int minodd=INT_MAX;
+        int mineven=INT_MAX;
+        for(int num: nums1){
+            if(num%2==0){
+                mineven=min(mineven, num);
+            }
+            else{
+                minodd=min(minodd, num);
+            }
+        }
+        if(minodd==INT_MAX){
+            return true;
+        }
+        if(mineven==INT_MAX){
+            return true;
+        }
+        return minodd<mineven;
+    }
+};
